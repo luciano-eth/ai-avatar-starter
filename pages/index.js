85fedfdc7709163e24e2b8260401dcfdc833e717
luciano-eth <113019900+luciano-eth@users.noreply.github.com>
2023-01-30 11:56:07 +0100

    Commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,104 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+    // Create state property
+  const maxRetries = 20;
+  const [input, setInput] = useState('');
+  const [img, setImg] = useState('');
+  // Numbers of retries
+  const [retry, setRetry] = useState(0);
+    // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState("");
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+};
+  
+  const generateAction = async () => {
+    console.log("Generating...");
+
+    if (isGenerating && retry === 0) return;
+
+    setIsGenerating(true);
+
+     // If this is a retry request, take away retryCount
+     if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+      }
+      // Add the fetch request
+      const response = await fetch('/api/generate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'image/jpeg',
+        },
+        body: JSON.stringify({ input }),
+      });
+    
+      const data = await response.json(); {
+    };
+
+    //If model still loading, drop that retry time
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    // If another error, drop error
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+
+    //Set image data into state property 
+    setFinalPrompt(input);
+    setInput('');
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
+        );
+        setRetryCount(maxRetries);
+        return;
+      }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+        await sleep(retry * 1000);
+        await generateAction();
+      };
+    
+      if (retry === 0) {
+        return;
+      }
+  
+      runRetry();
+    }, [retry]);
+
   return (
     <div className="root">
       <Head>
@@ -11,27 +107,53 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Avatar Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "leo" in the prompt</h2>
           </div>
-        </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className= "prompt-buttons">
+               <a
+                  className={
+                    isGenerating ? 'generate-button loading' : 'generate-button'
+                  }
+                  onClick={generateAction}
+               >
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                  <p>Generate</p>
+                  )}
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
-        </a>
+          {img && (
+  <div className="output-content">
+    <Image src={img} width={512} height={512} alt={finalPrompt} />
+    {/* Add prompt here */}
+    <p>{finalPrompt}</p>
+  </div>
+)}
+  </div>
+  <div className="badge-container grow">
+    <a
+      href="https://buildspace.so/builds/ai-avatar"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div className="badge">
+        <Image src={buildspaceLogo} alt="buildspace logo" />
+        <p>build with buildspace</p>
       </div>
-    </div>
-  );
+    </a>
+  </div>
+</div>
+  )       
 };
 
 export default Home;
